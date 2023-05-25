f45f248436003ade93f1b9a1d6f5cd7d135035de
Kent Gauen <gauenk@purdue.edu>
2023-05-25 10:05:46 -0400

    spynet frozen for first 30k

--- lib/nlnet/lightning.py
+++ lib/nlnet/lightning.py
@@ -44,6 +44,14 @@ import importlib
 # except:
 #     pass
 
+# # -- wandb --
+# WANDB_AVAIL = False
+# try:
+#     import wandb
+#     WANDB_AVAIL = True
+# except:
+#     pass
+
 # -- generic logging --
 import logging
 logging.basicConfig()
@@ -54,14 +62,17 @@ import pytorch_lightning as pl
 from pytorch_lightning import Callback
 from pytorch_lightning.loggers import CSVLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
-from pytorch_lightning.utilities.distributed import rank_zero_only
+from pytorch_lightning.utilities.rank_zero import rank_zero_only
 
+# import torch
+# torch.autograd.set_detect_anomaly(True)
 
 @econfig.set_init
 def init_cfg(cfg):
-    econfig.set_cfg(cfg)
-    cfgs = econfig({"lit":lit_pairs(),
-                    "sim":sim_pairs()})
+    econfig.init(cfg)
+    cfgs = econfig.extract_dict_of_pairs(cfg,{"lit":lit_pairs(),
+                                              "sim":sim_pairs()},
+                                         restrict=True)
     return cfgs
 
 def lit_pairs():
@@ -69,8 +80,16 @@ def lit_pairs():
              "isize":None,"bw":False,"lr_init":1e-3,
              "lr_final":1e-8,"weight_decay":0.,
              "nepochs":0,"task":"denoising","uuid":"",
-             "scheduler":"default","step_lr_size":5,
-             "step_lr_gamma":0.1,"flow_epoch":None,"flow_from_end":None}
+             "scheduler_name":"default","step_lr_size":5,
+             "step_lr_gamma":0.1,"flow_epoch":None,
+             "flow_from_end":None,"use_wandb":False,
+             "ntype":"g","rate":-1,"sigma":-1,
+             "sigma_min":-1,"sigma_max":-1,
+             "optim_name":"adam",
+             "sgd_momentum":0.1,"sgd_dampening":0.1,
+             "coswr_T0":-1,"coswr_Tmult":1,"coswr_eta_min":1e-9,
+             "step_lr_multisteps":"30-50",
+             "spynet_global_step":30000}
     return pairs
 
 def sim_pairs():
@@ -94,15 +113,16 @@ class LitModel(pl.LightningModule):
         lit_cfg = init_cfg(lit_cfg).lit
         for key,val in lit_cfg.items():
             setattr(self,key,val)
-        self.set_flow_epoch() # only for current exps; makes last 10 epochs with optical flow.
+        self.set_flow_epoch() # only for current exps; makes last 10 epochs with opt. flow.
         self.net = net
         self.sim_model = sim_model
         self.gen_loger = logging.getLogger('lightning')
         self.gen_loger.setLevel("NOTSET")
         self.automatic_optimization=True
 
-    def forward(self,vid):
-        flows = flow.orun(vid,self.flow,ftype=self.flow_method)
+    def forward(self,vid,flows=None):
+        if flows is None:
+            flows = flow.orun(vid,self.flow,ftype=self.flow_method)
         deno = self.net(vid,flows=flows)
         return deno
 
@@ -125,34 +145,67 @@ class LitModel(pl.LightningModule):
             self.flow = True
 
     def configure_optimizers(self):
-        optim = th.optim.Adam(self.parameters(),lr=self.lr_init,
-                              weight_decay=self.weight_decay)
+        if self.optim_name == "adam":
+            optim = th.optim.Adam(self.parameters(),lr=self.lr_init,
+                                  weight_decay=self.weight_decay)
+        elif self.optim_name == "sgd":
+            optim = th.optim.SGD(self.parameters(),lr=self.lr_init,
+                                 weight_decay=self.weight_decay,
+                                 momentum=self.sgd_momentum,
+                                 dampening=self.sgd_dampening)
+        else:
+            raise ValueError(f"Unknown optim [{self.optim_name}]")
         sched = self.configure_scheduler(optim)
         return [optim], [sched]
 
     def configure_scheduler(self,optim):
-        if self.scheduler in ["default","exp_decay"]:
-            gamma = 1-math.exp(math.log(self.lr_final/self.lr_init)/self.nepochs)
+        if self.scheduler_name in ["default","exp_decay"]:
+            gamma = math.exp(math.log(self.lr_final/self.lr_init)/self.nepochs)
             ExponentialLR = th.optim.lr_scheduler.ExponentialLR
             scheduler = ExponentialLR(optim,gamma=gamma) # (.995)^50 ~= .78
-        elif self.scheduler in ["step","steplr"]:
+            scheduler = {"scheduler": scheduler, "interval": "epoch", "frequency": 1}
+        elif self.scheduler_name in ["step","steplr"]:
             args = (self.step_lr_size,self.step_lr_gamma)
-            print("[Scheduler]: StepLR(%d,%2.2f)" % args)
+            # print("[Scheduler]: StepLR(%d,%2.2f)" % args)
             StepLR = th.optim.lr_scheduler.StepLR
             scheduler = StepLR(optim,step_size=self.step_lr_size,
                                gamma=self.step_lr_gamma)
-        elif self.scheduler in ["cos"]:
+        elif self.scheduler_name in ["cosa"]:
             CosAnnLR = th.optim.lr_scheduler.CosineAnnealingLR
-            T0,Tmult = 1,1
-            scheduler = CosAnnLR(optim,T0,Tmult)
-        elif self.scheduler in ["none"]:
+            scheduler = CosAnnLR(optim,self.nepochs)
+            scheduler = {"scheduler": scheduler, "interval": "epoch", "frequency": 1}
+        elif self.scheduler_name in ["cosa_step"]:
+            nsteps = self.num_steps()
+            CosAnnLR = th.optim.lr_scheduler.CosineAnnealingLR
+            scheduler = CosAnnLR(optim,nsteps)
+            scheduler = {"scheduler": scheduler, "interval": "step", "frequency": 1}
+        elif self.scheduler_name in ["multi_step"]:
+            milestones = [int(x) for x in self.step_lr_multisteps.split("-")]
+            MultiStepLR = th.optim.lr_scheduler.MultiStepLR
+            scheduler = MultiStepLR(optim,milestones=milestones,
+                                    gamma=self.step_lr_gamma)
+            scheduler = {"scheduler": scheduler, "interval": "epoch", "frequency": 1}
+        elif self.scheduler_name in ["coswr","cosw"]:
+            lr_sched =th.optim.lr_scheduler
+            CosineAnnealingWarmRestarts = lr_sched.CosineAnnealingWarmRestarts
+            # print(self.coswr_T0,self.coswr_Tmult,self.coswr_eta_min)
+            scheduler = CosineAnnealingWarmRestarts(optim,self.coswr_T0,
+                                                    T_mult=self.coswr_Tmult,
+                                                    eta_min=self.coswr_eta_min)
+            scheduler = {"scheduler": scheduler, "interval": "step", "frequency": 1}
+        elif self.scheduler_name in ["none"]:
             StepLR = th.optim.lr_scheduler.StepLR
             scheduler = StepLR(optim,step_size=10**3,gamma=1.)
         else:
-            raise ValueError(f"Uknown scheduler [{self.scheduler}]")
+            raise ValueError(f"Uknown scheduler [{self.scheduler_name}]")
         return scheduler
 
     def training_step(self, batch, batch_idx):
+
+        # -- check spynet --
+        if self.global_step > self.spynet_global_step:
+            if "spynet" in self.net:
+                self.net.spynet.train()
 
         # -- sample noise from simulator --
         self.sample_noisy(batch)
@@ -174,9 +227,15 @@ class LitModel(pl.LightningModule):
             cleans.append(clean_i)
         loss = loss / nbatches
 
+        # -- view params --
+        # loss.backward()
+        # for name, param in self.net.named_parameters():
+        #     if param.grad is None:
+        #         print(name)
+
         # -- append --
-        denos = th.stack(denos)
-        cleans = th.stack(cleans)
+        denos = th.cat(denos)
+        cleans = th.cat(cleans)
 
         # -- log --
         self.log("train_loss", loss.item(), on_step=True,
@@ -184,10 +243,19 @@ class LitModel(pl.LightningModule):
 
         # -- terminal log --
         val_psnr = np.mean(compute_psnrs(denos,cleans,div=1.)).item()
-        self.gen_loger.info("train_psnr: %2.2f" % val_psnr)
-        # print("train_psnr: %2.2f" % val_psnr)
+        # val_ssim = np.mean(compute_ssims(denos,cleans,div=1.)).item() # too slow.
         self.log("train_loss", loss.item(), on_step=True,
                  on_epoch=False, batch_size=self.batch_size)
+        self.log("train_psnr", val_psnr, on_step=True,
+                 on_epoch=False, batch_size=self.batch_size)
+        lr = self.optimizers()._optimizer.param_groups[-1]['lr']
+        self.log("lr", lr, on_step=True,
+                 on_epoch=False, batch_size=self.batch_size)
+        self.log("global_step", self.global_step, on_step=True,
+                 on_epoch=False, batch_size=self.batch_size)
+        # self.log("train_ssim", val_ssim, on_step=True,
+        #          on_epoch=False, batch_size=self.batch_size)
+        self.gen_loger.info("train_psnr: %2.2f" % val_psnr)
 
         return loss
 
@@ -196,10 +264,17 @@ class LitModel(pl.LightningModule):
         # -- unpack batch
         noisy = batch['noisy'][start:stop]/255.
         clean = batch['clean'][start:stop]/255.
-        # print("noisy.shape: ",noisy.shape)
+        fflow = batch['fflow'][start:stop]
+        bflow = batch['bflow'][start:stop]
+
+        # -- make flow --
+        if fflow.shape[-2:] == noisy.shape[-2:]:
+            flows = edict({"fflow":fflow,"bflow":bflow})
+        else:
+            flows = None
 
         # -- foward --
-        deno = self.forward(noisy)
+        deno = self.forward(noisy,flows)
 
         # -- report loss --
         loss = th.mean((clean - deno)**2)
@@ -212,15 +287,26 @@ class LitModel(pl.LightningModule):
 
         # -- denoise --
         noisy,clean = batch['noisy']/255.,batch['clean']/255.
+        val_index = batch['index'].cpu().item()
+
+        # -- flow --
+        fflow = batch['fflow']
+        bflow = batch['bflow']
+        if fflow.shape[-2:] == noisy.shape[-2:]:
+            flows = edict({"fflow":fflow,"bflow":bflow})
+        else:
+            flows = None
 
         # -- forward --
         gpu_mem.print_peak_gpu_stats(False,"val",reset=True)
         with th.no_grad():
-            deno = self.forward(noisy)
+            deno = self.forward(noisy,flows)
         mem_res,mem_alloc = gpu_mem.print_peak_gpu_stats(False,"val",reset=True)
 
         # -- loss --
         loss = th.mean((clean - deno)**2)
+        val_psnr = np.mean(compute_psnrs(deno,clean,div=1.)).item()
+        val_ssim = np.mean(compute_ssims(deno,clean,div=1.)).item()
 
         # -- report --
         self.log("val_loss", loss.item(), on_step=False,
@@ -229,10 +315,17 @@ class LitModel(pl.LightningModule):
                  on_epoch=True,batch_size=1,sync_dist=True)
         self.log("val_mem_alloc", mem_alloc, on_step=False,
                  on_epoch=True,batch_size=1,sync_dist=True)
-
-        # -- terminal log --
-        val_psnr = np.mean(compute_psnrs(deno,clean,div=1.)).item()
+        self.log("val_psnr", val_psnr, on_step=False,
+                 on_epoch=True,batch_size=1,sync_dist=True)
+        self.log("val_ssim", val_ssim, on_step=False,
+                 on_epoch=True,batch_size=1,sync_dist=True)
+        self.log("val_index", val_index, on_step=False,
+                 on_epoch=True,batch_size=1,sync_dist=True)
+        self.log("global_step",self.global_step,on_step=False,
+                 on_epoch=True,batch_size=1)
         self.gen_loger.info("val_psnr: %2.2f" % val_psnr)
+        self.gen_loger.info("val_ssim: %.3f" % val_ssim)
+
 
     def test_step(self, batch, batch_nb):
 
@@ -243,10 +336,18 @@ class LitModel(pl.LightningModule):
         index = float(batch['index'][0].item())
         noisy,clean = batch['noisy']/255.,batch['clean']/255.
 
+        # -- flow --
+        fflow = batch['fflow']
+        bflow = batch['bflow']
+        if fflow.shape[-2:] == noisy.shape[-2:]:
+            flows = edict({"fflow":fflow,"bflow":bflow})
+        else:
+            flows = None
+
         # -- forward --
         gpu_mem.print_peak_gpu_stats(False,"test",reset=True)
         with th.no_grad():
-            deno = self.forward(noisy)
+            deno = self.forward(noisy,flows)
         mem_res,mem_alloc = gpu_mem.print_peak_gpu_stats(False,"test",reset=True)
 
         # -- compare --
@@ -255,12 +356,14 @@ class LitModel(pl.LightningModule):
         ssim = np.mean(compute_ssims(deno,clean,div=1.)).item()
 
         # -- terminal log --
-        self.log("psnr", psnr, on_step=True, on_epoch=False, batch_size=1)
-        self.log("ssim", ssim, on_step=True, on_epoch=False, batch_size=1)
-        self.log("index", index,on_step=True,on_epoch=False,batch_size=1)
-        self.log("mem_res",  mem_res, on_step=True, on_epoch=False, batch_size=1)
-        self.log("mem_alloc",  mem_alloc, on_step=True, on_epoch=False, batch_size=1)
+        self.log("test_psnr", psnr, on_step=True, on_epoch=False, batch_size=1)
+        self.log("test_ssim", ssim, on_step=True, on_epoch=False, batch_size=1)
+        self.log("test_index", index,on_step=True,on_epoch=False,batch_size=1)
+        self.log("test_mem_res", mem_res, on_step=True, on_epoch=False, batch_size=1)
+        self.log("test_mem_alloc", mem_alloc,on_step=True,on_epoch=False,batch_size=1)
+        self.log("global_step",self.global_step,on_step=True,on_epoch=False,batch_size=1)
         self.gen_loger.info("te_psnr: %2.2f" % psnr)
+        self.gen_loger.info("te_ssim: %.3f" % ssim)
 
         # -- log --
         results = edict()
@@ -272,56 +375,15 @@ class LitModel(pl.LightningModule):
         results.test_index = index#.cpu().numpy().item()
         return results
 
-class MetricsCallback(Callback):
-    """PyTorch Lightning metric callback."""
-
-    def __init__(self):
-        super().__init__()
-        self.metrics = {}
-
-    def _accumulate_results(self,each_me):
-        for key,val in each_me.items():
-            if not(key in self.metrics):
-                self.metrics[key] = []
-            if hasattr(val,"ndim"):
-                ndim = val.ndim
-                val = val.cpu().numpy().item()
-            self.metrics[key].append(val)
-
-    @rank_zero_only
-    def log_metrics(self, metrics, step):
-        # metrics is a dictionary of metric names and values
-        # your code to record metrics goes here
-        print("logging metrics: ",metrics,step)
-
-    def on_train_epoch_end(self, trainer, pl_module):
-        each_me = copy.deepcopy(trainer.callback_metrics)
-        self._accumulate_results(each_me)
-
-    def on_validation_epoch_end(self, trainer, pl_module):
-        each_me = copy.deepcopy(trainer.callback_metrics)
-        self._accumulate_results(each_me)
-
-    def on_test_epoch_end(self, trainer, pl_module):
-        each_me = copy.deepcopy(trainer.callback_metrics)
-        self._accumulate_results(each_me)
-
-    def on_train_batch_end(self, trainer, pl_module, outs,
-                           batch, batch_idx, dl_idx):
-        each_me = copy.deepcopy(trainer.callback_metrics)
-        self._accumulate_results(each_me)
-
-
-    def on_validation_batch_end(self, trainer, pl_module, outs,
-                                batch, batch_idx, dl_idx):
-        each_me = copy.deepcopy(trainer.callback_metrics)
-        self._accumulate_results(each_me)
-
-    def on_test_batch_end(self, trainer, pl_module, outs,
-                          batch, batch_idx, dl_idx):
-        self._accumulate_results(outs)
-
-
+    def num_steps(self) -> int:
+        """Get number of steps"""
+        # Accessing _data_source is flaky and might break
+        dataset = self.trainer.fit_loop._data_source.dataloader()
+        dataset_size = len(dataset)
+        num_devices = max(1, self.trainer.num_devices)
+        acc = self.trainer.accumulate_grad_batches
+        num_steps = dataset_size * self.trainer.max_epochs // (acc * num_devices)
+        return num_steps
 
 def remove_lightning_load_state(state):
     names = list(state.keys())
