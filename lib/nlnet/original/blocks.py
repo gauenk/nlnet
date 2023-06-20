8fef904199c0bb66814fa242cdfe387736027c83
gauenk <kent.gauen@gmail.com>
2023-06-20 10:39:42 -0400

    update blocks; added v5

--- lib/nlnet/original/blocks.py
+++ lib/nlnet/original/blocks.py
@@ -2,6 +2,7 @@ from .block_v1 import Block
 from .block_v2 import BlockV2
 from .block_v3 import BlockV3
 from .block_v4 import BlockV4
+from .block_v5 import BlockV5
 
 def get_block_version(block_version):
     if block_version == "v1":
@@ -12,5 +13,7 @@ def get_block_version(block_version):
         return BlockV3
     elif block_version == "v4":
         return BlockV4
+    elif block_version == "v5":
+        return BlockV5
     else:
         raise ValueError("Uknown block type [%s]" % block_version)
--- lib/nlnet/original/scaling.py
+++ lib/nlnet/original/scaling.py
@@ -47,6 +47,52 @@ class Downsample(nn.Module):
         return flops
 
 # Upsample Block
+class UpsamplePixelShuffle(nn.Module):
+    def __init__(self, in_channel, out_channel):
+        super(Upsample, self).__init__()
+        kernel_size = 3
+        self.in_conv = nn.Conv2d(
+            in_channel, 4*in_channel, kernel_size,
+            padding=(kernel_size//2),stride=1, bias=True)
+        self.out_conv = nn.Conv2d(
+            in_channel, out_channel, kernel_size,
+            padding=(kernel_size//2),stride=1, bias=True)
+
+        self.deconv = nn.PixelShuffle(2)
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+
+    def extra_repr(self) -> str:
+        str_repr = "Upsample(in=%d,out=%d" % (self.in_channel,self.out_channel)
+        return str_repr
+
+    def forward(self, x):
+        # print("x.shape: ",x.shape)
+        B, T, C, H, W = x.shape
+        x = x.view(B*T,C,H,W)
+
+        # B, L, C = x.shape
+        # H = int(math.sqrt(L))
+        # W = int(math.sqrt(L))
+        # x = x.transpose(1, 2).contiguous().view(B, C, H, W)
+        x = self.in_conv(x)
+        # print("x.shape: ",x.shape)
+        x = self.deconv(x)#.flatten(2).transpose(1,2).contiguous() # B H*W C
+        # print("x.shape: ",x.shape)
+        out = self.out_conv(x)
+        # print("out.shape: ",out.shape)
+        BT,C,H,W = out.shape
+        out = out.view(B,T,C,H,W)
+        return out
+
+    def flops(self, H, W):
+        flops = 0
+        # conv
+        flops += H*2*W*2*self.in_channel*self.out_channel*2*2
+        # print("Upsample:{%.2f}"%(flops/1e9))
+        return flops
+
+# Upsample Block
 class Upsample(nn.Module):
     def __init__(self, in_channel, out_channel):
         super(Upsample, self).__init__()
